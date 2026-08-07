8e7501d6cb0803120d95dce66786c9e3b5add0a6
ritikanaik15489 <ritikanaik252@gmail.com>
2026-08-07 21:46:07 +0530

    Add files via upload

---
+++ code pratice.py
@@ -0,0 +1,144 @@
+# l1=[]
+# for i in range(2,11):
+#     for j in range(2,11):
+#         if i%j==0:
+#             break
+#         else:
+#             l1.append(i)
+#     print((set (l1)))
+
+# i=2 , j=2 = 0  
+
+# num1=5
+# sum1=0
+# for i in range(1,num1):
+#     if num1%i==0:
+#         print(i)
+#         sum1+=i
+# print("Sum of number is :",sum1)
+# if sum1==num1:
+#     print("Its an perfect number")
+# else:
+#     print("not perfect")
+    
+
+# num1=9
+# for i in range(2,num1):
+#     if num1%i==0:
+#         print(f"{num1} is an prime number")
+#         break
+#     else:
+#         print(f"{num1} is not a prime number")
+
+# str1="abc"
+# if str1==str1[::-1]:
+#     print("palindrone")
+# else:
+#     print("not palindrome")
+
+# num1=121
+# str1=str(num1)
+# if str1==str1[::-1]:
+#     print("palindrone")
+# else:
+#     print("not palindrome")
+
+# str2="aba"
+# rev_str=""
+# for i in str2:
+#     rev_str=i+rev_str
+# print(rev_str)
+# if str2==rev_str:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+
+# a=b+a = ba
+# ba=a+ba=aba
+
+# num1=5
+# fact=1
+# for i in range(1,num1+1):
+#     if i>0:
+#         fact=fact*i
+#     print(fact)
+
+# num1=6
+# sum=0
+# for i in range(1,num1):
+#     if num1%i==0:
+#         print(i)
+#         sum+=i
+# print("Sum of number is :",sum)
+# if num1==sum:
+#     print("perfect number")
+# else:
+#     print("not perfect number")
+
+# num2=4
+# fact=1
+# for i in range(1,num2+1):
+#     if i>0:
+#         fact*=i
+#         print(fact)
+
+# str1="abc"
+# if str1==str1[::1]:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# str2="mom"
+# rev_str=""
+# for i in str2:
+#     rev_str=i+rev_str
+# print(rev_str)
+# if str2==rev_str:
+#     print("palindrome")
+# else:
+#     print("not palindrome")
+
+# num1=0
+# num2=1
+# num3=10
+# for i in range(num3):
+#     print(num1,end=" ")
+#     temp=num1
+#     num1=num2
+#     num2=temp+num1
+
+# num1=0
+# num2=1
+# num3=10
+# for i in range(num3):
+#     print(num1,end=" ")
+#     num1,num2=num2,num1+num2
+
+# num1=153
+# str_num1=str(num1)
+# lennum1=len(str_num1)
+# sum=0
+# for i in str_num1:
+#     sum+=int(i)**lennum1
+# if num1==sum:
+#     print("Aremstrong")
+# else:
+#     print("not armstrong")
+
+# num1=10
+# for i in range(2,num1):
+#     if num1%i==0:
+#         print(f"{num1} is not prime")
+#         break
+# else:
+#     print(f"{num1} is a prime")
+
+# for i in range(2,10):
+#     for j in range(2,i):
+#         if i%j==0:
+#             break
+#     else:
+#         print(f"{i} is an prime")
+
+
