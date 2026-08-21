f1083fb2d0cc1c91f532d0dc459661773e304caf
ritikanaik15489 <ritikanaik252@gmail.com>
2026-08-21 11:07:47 +0530

    Add files via upload

---
+++ File handling.py
@@ -0,0 +1,10 @@
+# What is file - The file is a reference name to store actual data in your harddisk,pendrive,SSD etc..
+
+# Types of file
+# 1.Text File - readable(high level language), csv also an text file.
+# 2.Binary File - it is a binary data.
+
+# File Handling
+# We can manage original data inside the file like opening file closing and also writing and reading file.
+
+# Types of path
