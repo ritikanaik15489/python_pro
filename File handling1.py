a19d2bda64dc91e3b66bf9eabf41ae85ed43035c
ritikanaik15489 <ritikanaik252@gmail.com>
2026-08-21 19:43:45 +0530

    Add files via upload

---
+++ File handling1.py
@@ -0,0 +1,12 @@
+# What is file - The file is a reference name to store actual data in your harddisk,pendrive,SSD etc..
+
+# Types of file
+# 1.Text File - readable(high level language), csv also an text file.
+# 2.Binary File - it is a binary data.
+
+# File Handling
+# We can manage original data inside the file like opening file closing and also writing and reading file.
+
+# Types of path
+# Relative Paths: A path relative to the Current Working Directory (CWD) where your Python script is executing.Example: data/file.txt
+# Absolute Paths: The full, exact address of a file or directory starting from the root of your file system.Windows example: C:\Users\Name\Documents\file.txt
